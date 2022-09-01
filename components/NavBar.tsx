688efa1e70e533c8f8e06ef054eef2e45a3cbb46
Sammy Shear <sammy@sshear.dev>
2022-09-01 13:05:03 -0400

    Made the navbar buttons links

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+// @ts-ignore
+import  { useRouter } from "next/router";
 import styles from "styles/components/NavBar.module.scss";
 
 export default function NavBar() {
+
+    const router = useRouter();
+
     return (
         <header className={styles.bar}>
             <h2>Sammy Shear</h2>
-            <button className={styles.navBtn}>Home</button>
-            <button className={styles.navBtn}>About</button>
-            <button className={styles.navBtn}>Blog</button>
-            <button className={styles.navBtn}>Github</button>
-            <button className={styles.navBtn}>dev.to</button>
+            <button className={styles.navBtn} onClick={() => router.push("/")}>Home</button>
+            <button className={styles.navBtn} onClick={() => router.push("/about")}>About</button>
+            <button className={styles.navBtn} onClick={() => router.push("/blog")}>Blog</button>
+            <button className={styles.navBtn} onClick={() => router.push("https://github.com/sammyshear")}>Github</button>
+            <button className={styles.navBtn} onClick={() => router.push("https://dev.to/sammyshear")}>dev.to</button>
         </header>
     );
 }
