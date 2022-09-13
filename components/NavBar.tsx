7ffa0620824ddc7f61eab063de82befd5211f4f2
Sammy Shear <sammy@sshear.dev>
2022-09-13 10:59:25 -0400

    Added nvmrc to use node v16.17.0, updated navbar slightly

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-// @ts-ignore
-import  { useRouter } from "next/router";
+import {useRouter} from "next/router";
 import styles from "styles/components/NavBar.module.scss";
 
 export default function NavBar() {
@@ -10,11 +9,18 @@ export default function NavBar() {
     return (
         <header className={styles.bar}>
             <h2>Sammy Shear</h2>
-            <button className={styles.navBtn} onClick={() => router.push("/")}>Home</button>
-            <button className={styles.navBtn} onClick={() => router.push("/about")}>About</button>
-            <button className={styles.navBtn} onClick={() => router.push("/blog")}>Blog</button>
-            <button className={styles.navBtn} onClick={() => router.push("https://github.com/sammyshear")}>Github</button>
-            <button className={styles.navBtn} onClick={() => router.push("https://dev.to/sammyshear")}>dev.to</button>
+            <div className={styles.hr}>
+                <hr />
+            </div>
+            <div className={styles.btns}>
+                <button className={styles.navBtn} onClick={() => router.push("/")}>Home</button>
+                <button className={styles.navBtn} onClick={() => router.push("/about")}>About</button>
+                <button className={styles.navBtn} onClick={() => router.push("/blog")}>Blog</button>
+                <button className={styles.navBtn} onClick={() => router.push("https://github.com/sammyshear")}>Github
+                </button>
+                <button className={styles.navBtn} onClick={() => router.push("https://dev.to/sammyshear")}>dev.to
+                </button>
+            </div>
         </header>
     );
 }
